cadb1a3cea477c65dee0f02d91533ad25d5c5af7
OleksiiMatveichuk <126767121+OleksiiMatveichuk@users.noreply.github.com>
2023-05-09 22:22:51 +0300

    remake

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+const axios = require('axios');
 
+// Notiflix.Notify.success().failure().warning().info();
